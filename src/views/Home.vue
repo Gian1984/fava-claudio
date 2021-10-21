180a0bba529a9f906a74d02bdd1a26c1803bc056
Gianluca <gl.tiengo@gmail.com>
2021-10-21 12:55:30 +0200

    modify hover button color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,7 +41,7 @@
       <div class="relative shadow-xl sm:overflow-hidden">
 
         <div class="absolute inset-0">
-          <img class="h-full w-full object-cover" src="img/PRO_7942.jpg" alt="la cura del dettaglio" />
+          <img class="h-full w-full object-cover" src="img/PRO_7942.jpg" alt="il progetto degli spazi" />
           <div class="absolute inset-0" />
         </div>
 
@@ -59,7 +59,7 @@
                 Progettiamo coniugando estetica ed esigenze del cliente.<br>
               </p>
               <div class="text-right">
-                <a href="\Project_centrale_operativa_telecontrol" class="mt-2 md:mt-24 lg:mt-48 mb-2 bg-white opacity-50 hover:bg-blue-700 text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
+                <a href="\Project_centrale_operativa_telecontrol" class="mt-2 md:mt-24 lg:mt-48 mb-2 bg-white opacity-50 hover:bg-gray-700 hover:text-white text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
                   <span class="px-1 py-0.5">Vai al progetto</span>
                   <ChevronRightIcon class="w-5 h-5 text-gray-500" aria-hidden="true" />
                 </a>
@@ -83,7 +83,7 @@
       <div class="relative shadow-xl sm:overflow-hidden">
 
         <div class="absolute inset-0">
-          <img class="h-full w-full object-cover" src="img/rasoparete/vista-02-1-scaled.jpg" alt="la cura del dettaglio" />
+          <img class="h-full w-full object-cover" src="img/rasoparete/vista-02-1-scaled.jpg" alt="rifunzionalizzazione spazi" />
           <div class="absolute inset-0" />
         </div>
 
@@ -101,7 +101,7 @@
                 Progettiamo per dare nuova vita agli edifici.<br>
               </p>
               <div class="text-right">
-                <a href="\Project_concorso_rasoparete" class="mt-2 md:mt-24 lg:mt-48 mb-2 bg-white opacity-50 hover:bg-blue-700 text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
+                <a href="\Project_concorso_rasoparete" class="mt-2 md:mt-24 lg:mt-48 mb-2 bg-white opacity-50 hover:bg-gray-700 hover:text-white text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
                   <span class="px-1 py-0.5">Vai al progetto</span>
                   <ChevronRightIcon class="w-5 h-5 text-gray-500" aria-hidden="true" />
                 </a>
@@ -141,7 +141,7 @@
                 Progettiamo coniugando estetica ed esigenze del cliente.<br>
               </p>
               <div class="text-right">
-                <a href="\Project_oragiusta_moncalieri" class="mt-2 md:mt-24 lg:mt-48 mb-2 bg-white opacity-50 hover:bg-blue-700 text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
+                <a href="\Project_oragiusta_moncalieri" class="mt-2 md:mt-24 lg:mt-48 mb-2 bg-white opacity-50 hover:bg-gray-700 hover:text-white text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
                   <span class="px-1 py-0.5">Vai al progetto</span>
                   <ChevronRightIcon class="w-5 h-5 text-gray-500" aria-hidden="true" />
                 </a>
@@ -182,7 +182,7 @@
                 Disegniamo arredi personalizzati studiando ogni particolare.<br>
               </p>
               <div class="text-right">
-                <a href="\Project_casa_rivoli" class="mt-2 md:mt-24 lg:mt-48 mb-2 bg-white opacity-50 hover:bg-blue-700 text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
+                <a href="\Project_casa_rivoli" class="mt-2 md:mt-24 lg:mt-48 mb-2 bg-white opacity-50 hover:bg-gray-700 hover:text-white text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
                   <span class="px-1 py-0.5">Vai al progetto</span>
                   <ChevronRightIcon class="w-5 h-5 text-gray-500" aria-hidden="true" />
                 </a>
@@ -223,7 +223,7 @@
 
                 <ul role="list" class="flex justify-center space-x-5 pl-2 mt-4">
                   <li>
-                    <a :href="person.linkedinUrl" class="text-gray-400 hover:text-indigo-700">
+                    <a :href="person.linkedinUrl" class="text-gray-400 hover:text-blue-700">
                       <span class="sr-only">LinkedIn</span>
                       <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
