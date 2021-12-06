91db18fe4c72cb7f3feedca86a39415a9388aa11
Gianluca <gl.tiengo@gmail.com>
2021-12-06 11:44:41 +0100

    style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,13 +56,13 @@
             <div class="max-w-sm md:max-w-xs lg:max-w-xs xl:max-w-xs md:mr-0 md:w-1/2 lg:mr-0 md:ml-auto lg:ml-auto sm:mx-auto  lg:w-1/2 ">
               <div class="bg-white bg-opacity-50 lg:ml-12 p-2 mb-4">
                 <p class="mt-2 text-sm md:text-sm lg:text-base md:mt-10 lg:mt-6 max-w-lg text-sm md:text-sm text-black sm:max-w-3xl text-left ">
-                  Focus on:<br>
+                  FOCUS ON:<br>
                 </p>
-                <p class="text-base md:text-l lg:text-xl text-grey-500 sm:max-w-3xl text-left ">
+                <p class="pt-1 text-base md:text-l lg:text-xl text-grey-500 sm:max-w-3xl text-left ">
                   CASA RIVOLI
                 </p>
                 <p class="mt-2 text-sm md:text-sm lg:text-base md:mt-10 lg:mt-6 max-w-lg text-sm md:text-sm text-black sm:max-w-3xl text-left ">
-                  Il contrasto bianco e legno diventa il segno di continuità degli ambienti<br>
+                  Il contrasto bianco e legno diventa il segno di continuità degli ambienti.<br>
                 </p>
                 <div class="text-right">
                   <a href="\Project_casa_rivoli" class="mt-2 md:mt-24 lg:mt-44 mb-2 bg-white opacity-50 hover:bg-gray-700 hover:text-white text-xs text-black font-bold py-1 px-2 rounded-full inline-flex">
