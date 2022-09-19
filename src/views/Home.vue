fdcab4d4291f584abe68bbfb3f62da087f687c02
Gianluca Tiengo <gl.tiengo@gmail.com>
2022-09-19 14:29:34 +0200

    npm legacy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -336,7 +336,30 @@
 
   <!-- Projects Section -->
 
-  <Projects></Projects>
+  <div class="bg-white pb-5 enter" id="progetti">
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:py-10 sm:px-6 lg:px-12">
+
+      <div class="mx-auto py-3 px-4 text-center sm:px-6 lg:px-8 lg:py-3">
+        <h2 class="max-w-lg text-xl text-grey-500 sm:max-w-3xl mx-auto">PROGETTI</h2>
+      </div>
+
+      <div class="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-3 sm:gap-y-0 sm:gap-x-10 sm:gap-y-12 lg:gap-x-12">
+        <div v-for="product in products" :key="product.id" class="group relative">
+          <a :href="product.href">
+            <div class="h-full h-96 overflow-hidden group-hover:opacity-75">
+              <img :src="product.imageSrc" :alt="product.imageAlt" class="w-full h-full object-center object-cover" />
+            </div>
+          </a>
+          <p class="max-w-lg text-sm text-grey-500 sm:max-w-3xl">
+            <a :href="product.href" class=" mb-2 inline-flex items-center uppercase text-black rounded-full p-1 pr-2 sm:text-xs lg:text-sm xl:text-base hover:text-gray-500">
+              <span class="absolute inset-0" />
+              {{ product.name }}
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 
@@ -345,10 +368,10 @@
 
 
   <div class="bg-white">
-    <div class="mx-auto py-12 px-4 sm:px-6 lg:px-8 lg:py-12">
+    <div class="mx-auto py-12 px-4 sm:px-6 lg:px-8 lg:py-12" id="press">
       <div class="space-y-12">
         <div class="space-y-5 mx-auto text-center">
-          <h2 class=" text-xl mx auto text-center text-gray-900 tracking-tight" id="press">PRESS</h2>
+          <h2 class=" text-xl mx auto text-center text-gray-900 tracking-tight">PRESS</h2>
           <p class="text-gray-900 max-w-3xl mx-auto">
           </p>
         </div>
@@ -390,7 +413,6 @@
 
 <script>
 
-import Projects from "../components/Projects";
 import { ChevronRightIcon, ArrowCircleRightIcon } from '@heroicons/vue/solid'
 import { gsap } from 'gsap';
 
@@ -437,6 +459,93 @@ const press = [
     HrefUrl: '/Press',
   },
 ]
+
+
+
+const projects = [
+
+  {
+    id: 1,
+    name: 'Casa Gramsci',
+    href: '/Project_casa_gramsci',
+    imageSrc: 'img/casagramsci/01.webp',
+    imageAlt: "Casa Gramsci.",
+  },
+
+  {
+    id: 2,
+    name: 'Concorso Rasoparete',
+    href: '/Project_concorso_rasoparete',
+    imageSrc: 'img/rasoparete/vista-02-1-scaled.jpg',
+    imageAlt: "Concorso Rasoparete.",
+  },
+  {
+    id: 3,
+    name: "L'oragiusta Moncalieri",
+    href: '/Project_oragiusta_moncalieri',
+    imageSrc: 'img/loragiusta/foto-1-scaled.jpg',
+    imageAlt: "L'oragiusta Moncalieri.",
+  },
+  {
+    id: 4,
+    name: "Vivere in 20 mq",
+    href: '/Project_vivere_20',
+    imageSrc: 'img/vivere20metri/03-scaled.jpg',
+    imageAlt: "Vivere in 20 mq",
+  },
+  {
+    id: 5,
+    name: 'Sala Congressi Telecontrol',
+    href: '/Project_sala_congressi_telecontrol',
+    imageSrc: 'img/vista-01.jpg',
+    imageAlt: "Sala Congressi Telecontrol.",
+  },
+  {
+    id: 6,
+    name: 'Casa Nel Parco',
+    href: '/Project_casa_nel_parco',
+    imageSrc: 'img/vista-esterna-02_crop.jpg',
+    imageAlt: "Casa Nel Parco.",
+  },
+  {
+    id: 7,
+    name: 'Villa Perrault',
+    href: '/Project_villa_perrault',
+    imageSrc: 'img/villaperrault/vista-02-scaled.jpg',
+    imageAlt: "Villa Perrault.",
+  },
+  {
+    id: 8,
+    name: 'Casa Rivoli',
+    href: '/Project_casa_rivoli',
+    imageSrc: 'img/PRO_7711.jpg',
+    imageAlt: "Casa Rivoli.",
+  },
+  {
+    id: 9,
+    name: 'Centrale Operativa Telecontrol',
+    href: '/Project_centrale_operativa_telecontrol',
+    imageSrc: 'img/PRO_7906.jpg',
+    imageAlt: "Centrale Operativa Telecontrol.",
+  },
+  {
+    id: 10,
+    name: 'Casa R',
+    href: '/Project_casa_R',
+    imageSrc: 'img/vista-02-versione-A.jpg',
+    imageAlt: "Casa R.",
+  },
+  {
+    id: 11,
+    name: 'Camera Dei Bimbi',
+    href: '/Project_camera_bimbi',
+    imageSrc: 'img/vista-03_crop.jpg',
+    imageAlt: "Camera Dei Bimbi.",
+  },
+]
+
+
+
 
 const products = [
   {
@@ -510,7 +619,6 @@ export default {
   components: {
     ChevronRightIcon,
     ArrowCircleRightIcon,
-    Projects,
   },
 
   setup() {
@@ -533,7 +641,8 @@ export default {
       enter,
       products,
       team,
-      press
+      press,
+      projects
     }
   },
 }
